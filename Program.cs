dcc0b7f9cbc700baa39339233760d8089279a2c0
Tony <antony.branquinho@gmail.com>
2024-01-05 11:08:44 -0300

    basico

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva um número inteiro");
+            int numero = int.Parse(Console.ReadLine());
+
+            string saida = "nulo";
+            if ((numero % 2) != 0) {
+                saida = "impar";
+            }
+            else {
+                saida = "par";
+            }
+
+            Console.WriteLine(saida);
+        }
+    }
+}
